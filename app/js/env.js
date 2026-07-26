3ef587f8a8bab9a799b85fb0d90bf6364bba6b83
bts <bizarrototalsolutions@gmail.com>
2026-07-26 21:23:06 +0100

    Update env.js

--- app/js/env.js
+++ app/js/env.js
@@ -1,1 +1,3 @@
 
+window.SUPABASE_URL = 'https://vjbvjzmxbeoyflhrwrpy.supabase.co';       // Project Settings → API → Project URL
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqYnZqem14YmVveWZsaHJ3cnB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ4MzAzMjcsImV4cCI6MjEwMDQwNjMyN30.XC7m4KXLqdif6AeoskiAckjvTpKCZIiI3Nza_FoY0qY';                  // Project Settings → API → anon public key
